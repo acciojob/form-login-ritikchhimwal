d639918ee91c6a85f5f1e0ffda50ea675c2ed276
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-11-20 22:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+
+    // Get the form element
+    const form = document.getElementById('form1');
+
+    // Get the values of the input fields
+    const firstName = form.elements['fname'].value;
+    const lastName = form.elements['lname'].value;
+
+    // Display the full name using an alert
+    alert(`${firstName} ${lastName}`);
 
 }
